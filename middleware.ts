f925e2aa3a94303214ee87d578b71331b6071666
Lucas Duarte <126193564+LucasDuarte42@users.noreply.github.com>
2026-05-05 19:19:24 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,9 @@
-// import { withAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
 
-// export default withAuth({
-//   pages: {
-//     signIn: '/login',
-//   },
-// })
+export function middleware() {
+  return NextResponse.next()
+}
 
-// export const config = {
-//   matcher: ['/dashboard/:path*', '/mangas/:path*'],
-// }
-
-export {}
+export const config = {
+  matcher: [],
+}
